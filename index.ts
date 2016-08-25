d5f5a2fa7bcec9899766c5eedcd1e61cf7767e36
cody <cevek@ya.ru>
2016-08-25 17:31:20 +0500

    Use global variable instead of local because maybe different versions of install

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-const diMap = new Map();
+declare const global: any;
+if (!global.__diMap){
+    global.__diMap = new Map();
+}
 export function inject<T>(Class: new ()=>T): T {
+    const diMap = global.__diMap as Map<{}, T>;
     let val = diMap.get(Class);
     if (val) {
         return val;
@@ -10,5 +14,6 @@ export function inject<T>(Class: new ()=>T): T {
 }
 
 export function bindInjection<T>(Class: new ()=>T, value: T) {
+    const diMap = global.__diMap as Map<{}, T>;
     diMap.set(Class, value);
 }
